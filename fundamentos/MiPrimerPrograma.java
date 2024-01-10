80e6efb8b2079d7f701f8adc8f327c4bfe602c77
RomanTecpa <rtecpaperez@gmail.com>
2024-01-10 14:41:05 -0600

    Fundamentos de POO

---
+++ fundamentos/MiPrimerPrograma.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class MiPrimerPrograma {
+	public static void main(String[] args) {
+		// Practica No.1     Grupo  5'I'
+		// Objetivo: Mi primer programa \
+		// Fundamentos de programacion orientada a objetos 
+		// Integrantes: Roman Tecpa Perez, Alan Mendez Hernadez, Miguel Anguel Mata Tehozol
+		System.out.println("Hola Mundo, Bienvenido al maravilloso mundo de JAVA");
+	}
+
+}
