f2d294fda9e73ab05f70527ea06c8a1957306b7a
roman-tecpa <136993955+roman-tecpa@users.noreply.github.com>
2024-01-18 22:03:24 -0600

    Add files via upload

---
+++ fundamentos/Carro.java
@@ -0,0 +1,69 @@
+package Mantenimiento;
+
+public class Carro {
+private String marca;
+private String modelo;
+private String color;
+private int puertas;
+private int cilindros;
+private int anio;
+private String placas;
+private Double kmRecorridos;
+private Double litGas;
+
+public String getMarca() {
+	return marca;
+}
+public void setMarca(String marca) {
+	this.marca = marca;
+}
+public String getModelo() {
+	return modelo;
+}
+public void setModelo(String modelo) {
+	this.modelo = modelo;
+}
+public String getColor() {
+	return color;
+}
+public void setColor(String color) {
+	this.color = color;
+}
+public int getPuertas() {
+	return puertas;
+}
+public void setPuertas(int puertas) {
+	this.puertas = puertas;
+}
+public int getCilindros() {
+	return cilindros;
+}
+public void setCilindros(int cilindros) {
+	this.cilindros = cilindros;
+}
+public int getAnio() {
+	return anio;
+}
+public void setAnio(int anio) {
+	this.anio = anio;
+}
+public String getPlacas() {
+	return placas;
+}
+public void setPlacas(String placas) {
+	this.placas = placas;
+}
+public Double getKmRecorridos() {
+	return kmRecorridos;
+}
+public void setKmRecorridos(Double kmRecorridos) {
+	this.kmRecorridos = kmRecorridos;
+}
+public Double getLitGas() {
+	return litGas;
+}
+public void setLitGas(Double litGas) {
+	this.litGas = litGas;
+}
+
+}
