946990828dec3ca31af8f04240c9cf0a05ac3c55
roman-tecpa <136993955+roman-tecpa@users.noreply.github.com>
2024-01-20 23:34:21 -0600

    Add files via upload

--- fundamentos/Clase_carro/Carro.java
+++ fundamentos/Clase_carro/Carro.java
@@ -1,117 +1,87 @@
 package Mantenimiento;
 
-public class Carro {
-    private String marca;
-    private String modelo;
-    private String color;
-    private int puertas;
-    private int cilindros;
-    private int anio;
-    private String placas;
-    private Double kmRecorridos;
-    private Double litGas;
+public class Carro{
+	
+	private static int numCoche = 0;
+	
+	private String marca;
+	private int VelActual;
+	private String Modelo;
+	private int anio;
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public int getVelActual() {
+		return VelActual;
+	}
+	
+	public void setVelActual(int velActual) {
+		VelActual = velActual;
+	}
+	
+	public String getModelo() {
+		return Modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+	
+	public int getAnio() {
+		return anio;
+	}
+	
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	
+	public Carro() {
+		contarCoche();
+	}
+	
+	public Carro(String marca, String modelo, int anio, int velActual) {
+		this.marca = marca;
+		this.Modelo = modelo;
+		this.anio = anio;
+		this.VelActual = velActual;
+		contarCoche();
+	}
+	
+	static void contarCoche() {
+		numCoche++;
+	}
+	
+	public static int getNumCoche() {
+		return numCoche;
+	}
+	
+	public static void main(String[] args) {
+	    Carro carro1 = new Carro();
+	    carro1.setMarca("Tesla");
+	    carro1.setModelo("X1");
+	    carro1.setVelActual(300);
+	    System.out.println("Este es el carro: " + Carro.getNumCoche());
+	    System.out.println("La marca del carro1 es: " + carro1.getMarca());
+	    System.out.println("El modelo del carro1 es: " + carro1.getModelo());
+	    System.out.println("La velocidad del carro1 es: " + carro1.getVelActual());
 
-    public String getMarca() {
-        return marca;
-    }
+	    Carro carro2 = new Carro("nissan", "Gtr", 2001, 400);
+	    System.out.println("\nEste es el carro: " + Carro.getNumCoche());
+	    System.out.println("La marca del carro2 es: " + carro2.getMarca());
+	    System.out.println("El modelo del carro2 es: " + carro2.getModelo());
+	    System.out.println("La velocidad del carro2 es: " + carro2.getVelActual());
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+	    Carro carro3 = new Carro("Chevrolet", "Chevy", 2002, 180);
+	    System.out.println("\nEste es el carro: " + Carro.getNumCoche());
+	    System.out.println("La marca del carro3 es: " + carro3.getMarca());
+	    System.out.println("El modelo del carro3 es: " + carro3.getModelo());
+	    System.out.println("La velocidad del carro es: " + carro3.getVelActual());
+	}
 
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getPuertas() {
-        return puertas;
-    }
-
-    public void setPuertas(int puertas) {
-        this.puertas = puertas;
-    }
-
-    public int getCilindros() {
-        return cilindros;
-    }
-
-    public void setCilindros(int cilindros) {
-        this.cilindros = cilindros;
-    }
-
-    public int getAnio() {
-        return anio;
-    }
-
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-
-    public String getPlacas() {
-        return placas;
-    }
-
-    public void setPlacas(String placas) {
-        this.placas = placas;
-    }
-
-    public Double getKmRecorridos() {
-        return kmRecorridos;
-    }
-
-    public void setKmRecorridos(Double kmRecorridos) {
-        this.kmRecorridos = kmRecorridos;
-    }
-
-    public Double getLitGas() {
-        return litGas;
-    }
-
-    public void setLitGas(Double litGas) {
-        this.litGas = litGas;
-    }
-
-    public static void main(String[] args) {
-        Carro Carro1 =  new Carro();
-		System.out.println("ESTE ES EL CARRO 1");
-		Carro1.setAnio(2000);
-		Carro1.setColor("Rosa");
-		Carro1.setModelo("Nissan GTR");
-		
-		System.out.println("AÑO DEL CARRO" + Carro1.getAnio());
-		System.out.println("COLOR DEL CARRO: "+Carro1.getColor());
-		System.out.println("MODELO DE CARRO: "+Carro1.getModelo()+ "\n");
-		
-		
-		Carro Carro2 = new Carro();
-
-		System.out.println("ESTE ES EL CARRO 2");
-		Carro2.setAnio(2019);
-		Carro2.setCilindros(4);
-		Carro2.setModelo("Tsuru");
-		System.out.println("AÑO DEL CARRO: "+Carro1.getAnio());
-		System.out.println("NUMERO DE CILINDROS DEL CARRO: "+Carro1.getCilindros());
-		System.out.println("MODELO DE CARRO: "+Carro1.getModelo()+ "\n");
-		
-		Carro Carro3 = new Carro();
-		System.out.println("ESTE ES EL CARRO 3");
-		Carro3.setMarca("Chevrolet");
-		Carro3.setPlacas("74-08-RA");
-		Carro3.setPuertas(4);
-		System.out.println("MARCA DEL CARRO: "+Carro3.getMarca());
-		System.out.println("PLACAS DEL CARRO: "+Carro3.getPlacas());
-		System.out.println("NUMERO DE PUERTAS DEL CARRO: "+Carro3.getPuertas());
-    }
 }
